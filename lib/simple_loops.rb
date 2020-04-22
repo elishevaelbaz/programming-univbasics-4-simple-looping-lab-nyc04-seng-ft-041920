7f8024269ebdf68eb418eec68b74cc1a37808394
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-22 15:41:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,3 +16,12 @@ def loop_message_n_times(string, n)
     counter +=1
   end
 end
+
+def output_array(array)
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
+    counter +=1
+  end
+end
+end
